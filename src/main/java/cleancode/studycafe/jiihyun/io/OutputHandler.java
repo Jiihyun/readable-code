72b5983026d8bef48a357c1449bbde708a73e4a1
Jiihyun <cwlgusc@gmail.com>
2025-03-11 20:00:32 +0900

    refactor: OutputHandler 추상화

---
+++ src/main/java/cleancode/studycafe/jiihyun/io/OutputHandler.java
@@ -0,0 +1,24 @@
+package cleancode.studycafe.jiihyun.io;
+
+import cleancode.studycafe.jiihyun.model.StudyCafeLockerPass;
+import cleancode.studycafe.jiihyun.model.StudyCafePass;
+
+import java.util.List;
+
+public interface OutputHandler {
+
+    void showWelcomeMessage();
+
+    void showAnnouncement();
+
+    void askPassTypeSelection();
+
+    void showPassListForSelection(List<StudyCafePass> passes);
+
+    void askLockerPass(StudyCafeLockerPass lockerPass);
+
+    void showPassOrderSummary(StudyCafePass selectedPass, StudyCafeLockerPass lockerPass);
+
+    void showSimpleMessage(String message);
+
+}
