484650bfee747f203b7daf29f709a8a85fd01022
Jiihyun <cwlgusc@gmail.com>
2025-03-12 00:11:04 +0900

    refactor: StudyCafeFileHandler 리팩토링 - path 상수화 - 파일에 있는 라인 파싱 하는 책임 분리

--- src/main/java/cleancode/studycafe/jiihyun/io/StudyCafeFileHandler.java
+++ src/main/java/cleancode/studycafe/jiihyun/io/StudyCafeFileHandler.java
@@ -2,55 +2,45 @@ package cleancode.studycafe.jiihyun.io;
 
 import cleancode.studycafe.jiihyun.model.StudyCafeLockerPass;
 import cleancode.studycafe.jiihyun.model.StudyCafePass;
-import cleancode.studycafe.jiihyun.model.StudyCafePassType;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudyCafeFileHandler {
 
+    private static final String PATH_OF_PASS_LIST = "src/main/resources/cleancode/studycafe/pass-list.csv";
+    private static final String PATH_OF_LOCKER = "src/main/resources/cleancode/studycafe/locker.csv";
+
     public List<StudyCafePass> readStudyCafePasses() {
         try {
-            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/cleancode/studycafe/pass-list.csv"));
-            List<StudyCafePass> studyCafePasses = new ArrayList<>();
-            for (String line : lines) {
-                String[] values = line.split(",");
-                StudyCafePassType studyCafePassType = StudyCafePassType.valueOf(values[0]);
-                int duration = Integer.parseInt(values[1]);
-                int price = Integer.parseInt(values[2]);
-                double discountRate = Double.parseDouble(values[3]);
-
-                StudyCafePass studyCafePass = StudyCafePass.of(studyCafePassType, duration, price, discountRate);
-                studyCafePasses.add(studyCafePass);
-            }
-
-            return studyCafePasses;
+            final List<String> lines = Files.readAllLines(Paths.get(PATH_OF_PASS_LIST));
+            return getStudyCafePasses(lines);
         } catch (IOException e) {
             throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
         }
     }
 
+    private List<StudyCafePass> getStudyCafePasses(final List<String> lines) {
+        return lines.stream()
+                .map(FileLineParser::parsePassList)
+                .toList();
+    }
+
     public List<StudyCafeLockerPass> readLockerPasses() {
         try {
-            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/cleancode/studycafe/locker.csv"));
-            List<StudyCafeLockerPass> lockerPasses = new ArrayList<>();
-            for (String line : lines) {
-                String[] values = line.split(",");
-                StudyCafePassType studyCafePassType = StudyCafePassType.valueOf(values[0]);
-                int duration = Integer.parseInt(values[1]);
-                int price = Integer.parseInt(values[2]);
-
-                StudyCafeLockerPass lockerPass = StudyCafeLockerPass.of(studyCafePassType, duration, price);
-                lockerPasses.add(lockerPass);
-            }
-
-            return lockerPasses;
+            final List<String> lines = Files.readAllLines(Paths.get(PATH_OF_LOCKER));
+            return getStudyCafeLockerPass(lines);
         } catch (IOException e) {
             throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
         }
+    }
+
+    private List<StudyCafeLockerPass> getStudyCafeLockerPass(final List<String> lines) {
+        return lines.stream()
+                .map(FileLineParser::parseLocker)
+                .toList();
     }
 
 }
