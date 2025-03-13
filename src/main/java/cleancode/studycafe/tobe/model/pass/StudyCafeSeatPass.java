9fee613d7cb0075556cd5dcef6e54bddc196d5c8
Jiihyun <cwlgusc@gmail.com>
2025-03-13 20:56:26 +0900

    refactor: PassOrder 도메인 추출

--- src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
+++ src/main/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPass.java
@@ -33,6 +33,10 @@ public class StudyCafeSeatPass implements StudyCafePass {
         return this.passType.isNotLockerType();
     }
 
+    public int getDiscountPrice() {
+        return (int) (price * discountRate);
+    }
+
     @Override
     public StudyCafePassType getPassType() {
         return passType;
@@ -46,10 +50,6 @@ public class StudyCafeSeatPass implements StudyCafePass {
     @Override
     public int getPrice() {
         return price;
-    }
-
-    public double getDiscountRate() {
-        return discountRate;
     }
 
 }
